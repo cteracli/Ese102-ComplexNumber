55ec3cf9cc112cf528a10564129b3e794e6da562
Caterina Eracli <eracli.caterina@gmail.com>
2015-03-04 23:22:27 +0100

    Rinominata Classe

--- src/complexnumber/ComplexNumber.java
+++ src/complexnumber/ComplexNumber.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ese102.complexnumber;
+package complexnumber;
 
 /**
  *
  * @author Caterina Eracli
  */
-public class Ese102ComplexNumber {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class ComplexNumber {
     
 }
