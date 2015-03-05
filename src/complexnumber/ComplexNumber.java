bb8d9c9b6ff73a9515b610e81daced986efbfece
Caterina Eracli <eracli.caterina@gmail.com>
2015-03-05 23:07:09 +0100

    Commit Finale

--- src/complexnumber/ComplexNumber.java
+++ src/complexnumber/ComplexNumber.java
@@ -11,4 +11,43 @@ package complexnumber;
  */
 public class ComplexNumber {
     
+    private double re;
+    private double im;
+    public ComplexNumber(){
+        this.re=0;
+        this.im=0;
+    }
+    public double getRe(){
+        return re;
+    }
+    public double getIm(){
+        return im;
+    }
+    public double getModulus(){
+        double modulus=(float) Math.sqrt(((float)Math.pow(this.im, 2))+((float)Math.pow(this.re, 2)));
+        return modulus;
+    }
+    public double getArgument(){
+        double argument=0;
+        if(getRe()!=0 && getIm()!=0){
+            argument= Math.atan(this.im/this.re);
+        }
+        return Math.toDegrees(argument);
+    }
+    public void setPolar(double mod, double arg){
+        this.re=mod*((float)Math.cos(Math.toRadians(arg)));
+        this.im=mod*((float)Math.sin(Math.toRadians(arg)));
+    }
+    public void setRectangular(double im, double re){
+        this.im=im;
+        this.re=re;
+    }
+    public String stampaRectangular(){
+        String r = this.getRe() + "+(" + this.getIm() + ")i";
+        return r;
+    }
+    public String stampaPolar(){
+        String r = this.getModulus() + "+(" + this.getArgument() + ")i";
+        return r;
+    }
 }
--- src/ese102/Ese102.java
+++ src/ese102/Ese102.java
@@ -9,13 +9,87 @@ package ese102;
  *
  * @author Caterina Eracli
  */
+import java.io.*;
+import complexnumber.*;
+
 public class Ese102 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader tastiera = new BufferedReader(input);        
+        
+        String linea = "";
+	ComplexNumber a=new ComplexNumber();
+        double re=0;
+        boolean valoreValido = false;
+        while(!valoreValido) {
+            System.out.print("Inserisci numero reale: ");
+            linea = tastiera.readLine();
+            try {
+                re=Double.valueOf(linea).doubleValue();
+                valoreValido=true;
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Numero non valido");
+            }
+        }
+        
+        valoreValido=false;
+	double im=0;
+        while(!valoreValido) {
+            System.out.print("Inserisci numero immaginario: ");
+            linea = tastiera.readLine();
+            try {
+                im=Double.valueOf(linea).doubleValue();
+                valoreValido=true;
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Numero non valido");
+            }
+        }
+        
+        a.setRectangular(im, re);
+	System.out.println("Coordinate polari: " + a.stampaPolar());
+        System.out.println("Coordinate cartesiane: " + a.stampaRectangular());
+        
+	double arg=0;
+         valoreValido=false;
+        while(!valoreValido) {
+            System.out.print("Inserisci argomento: ");
+            linea = tastiera.readLine();
+            try {
+                arg=Double.valueOf(linea).doubleValue();
+                valoreValido=true;
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Numero non valido");
+            }
+        }
+        
+        valoreValido=false;
+	double mod= 0;
+        while(!valoreValido) {
+            System.out.print("Inserisci modulo: ");
+            linea=tastiera.readLine();
+            try {
+                mod=Double.valueOf(linea).doubleValue();
+                valoreValido=true;
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Numero non valido");
+            }
+        }
+
+	a.setPolar(mod, arg);
+	System.out.println("Coordinate polari: " + a.stampaPolar());
+        System.out.println("Coordinate cartesiane: " + a.stampaRectangular
+        ());
+
+
     }
+
     
 }
